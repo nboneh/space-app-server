407fd87bea9e93ae3eaca255c2f4b071a73554a8
Nir Boneh <nboneh@asahi.ngdc.noaa.gov>
2014-06-03 15:36:39 -0600

    first commit

---
+++ src/com/clouby/retrospace/server/HighScore.java
@@ -0,0 +1,81 @@
+package com.clouby.retrospace.server;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+
+
+
+public class HighScore extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3961076477647464957L;
+	static final String ENTITY_NAME = "HighScoreRecord";
+	static final String PASSWORD = "*******";
+	
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query(ENTITY_NAME);
+		PreparedQuery pq = datastore.prepare(query);
+		JSONObject jsonEntity = new JSONObject(); 
+		JSONArray jsonEntityArray = new JSONArray(); 
+
+		for (Entity result : pq.asIterable()) {
+			long score = (long) result.getProperty("score");
+			String name  = (String) result.getProperty("name");
+		
+			try {
+				jsonEntity.put("score", score);
+				jsonEntity.put("name", name);
+				jsonEntityArray.put(jsonEntity);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			jsonEntity = new JSONObject(); 
+		}
+		
+	    try {
+			jsonEntityArray.write(resp.getWriter());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	    
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		if(req.getParameter("pass").equals(PASSWORD)){
+			// Places the location parameters in the same entity group as the Location record
+			Entity highscoreInst = new Entity(ENTITY_NAME);
+
+
+			highscoreInst.setProperty("date", new Date(Long.parseLong(req.getParameter("date"))));
+			highscoreInst.setProperty("name", req.getParameter("name"));
+			highscoreInst.setProperty("score", Integer.parseInt("score"));
+		
+			// Now put the entry to Google data store
+			DatastoreService datastore =
+					DatastoreServiceFactory.getDatastoreService();
+			datastore.put(highscoreInst);
+		}
+	}
+}
